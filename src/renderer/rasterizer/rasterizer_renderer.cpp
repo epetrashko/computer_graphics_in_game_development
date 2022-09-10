28705979641bf73dd8cceeb246d3050092f58a5c
epetrashko <Lopory_324!>
2022-09-10 13:27:25 +0300

    Added depth buffer for rasterization windows

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -15,7 +15,7 @@ void cg::renderer::rasterization_renderer::init()
 	rasterizer->set_viewport(settings->width, settings->height);
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 			settings->width, settings->height);
-	rasterizer->set_render_target(render_target);
+
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
 
@@ -33,6 +33,9 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_angle_of_view(settings->camera_angle_of_view);
 	camera->set_z_near(settings->camera_z_near);
 	camera->set_z_far(settings->camera_z_far);
+
+	depth_buffer = std::make_shared<resource<float>>(settings->width, settings->height);
+	rasterizer->set_render_target(render_target, depth_buffer);
 }
 void cg::renderer::rasterization_renderer::render()
 {
