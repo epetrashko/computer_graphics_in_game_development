9528d6fa857486265ddefb75174c985c5a75508d
epetrashko <Lopory_324!>
2022-09-30 17:09:43 +0300

    Implement aabb class

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -70,7 +70,12 @@ void cg::renderer::ray_tracing_renderer::render()
 		float3 position = ray.position + payload.t *  ray.direction;
 		for (auto& light: lights){
 			cg::renderer::ray to_light(position, light.position - position);
-			result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			auto shadow_payload = shadow_raytracer->trace_ray(
+					to_light, 1, length(light.position - position)
+					);
+			if (shadow_payload.t < 0.f){
+				result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+			}
 		}
 		payload.color = cg::color::from_float3(result_color);
 		return payload;
